689322173bb4a451aad15cba9e37c465fd9d145c
jbachurski <kbachurski@gmail.com>
2024-02-09 23:30:38 +0000

    Actually use timer timeout callbacks

--- entities/player/playerrefl.gd
+++ entities/player/playerrefl.gd
@@ -2,9 +2,10 @@ extends Area2D
 
 const BULLET = preload("res://entities/projectiles/bullet.tscn")
 
-func _process(delta):
-	if $ShootTimer.time_left <= 0:
-		var b = BULLET.instantiate()
-		get_parent().get_parent().add_child(b)
-		b.start(position, Vector2(0, -1), false)
-		$ShootTimer.start()
+func _on_shoot_timer_timeout():
+	var b = BULLET.instantiate()
+	get_parent().get_parent().add_child(b)
+	b.start(position, Vector2(0, -1), false)
+
+func _ready():
+	$ShootTimer.connect("timeout", _on_shoot_timer_timeout)
