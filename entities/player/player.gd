92f2a8155b55d86ffa2933d03072c7e83648b39e
jbachurski <kbachurski@gmail.com>
2024-02-10 17:34:31 +0000

    Change the speed scale

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -5,7 +5,7 @@ var speed_level = 0
 
 func _process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position = position + delta * direction * speed * pow(1.5, speed_level)
+	position = position + delta * direction * speed * sqrt(1 + 0.3 * speed_level)
 	var size = get_viewport_rect().size
 	position.x = clamp(position.x, 0, size.x)
 	position.y = clamp(position.y, 0, size.y)
