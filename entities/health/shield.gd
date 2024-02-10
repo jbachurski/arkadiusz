50b5cb2b3cec6d4d3f9b175769ae2eadd462a68f
spdskatr <25078279+spdskatr@users.noreply.github.com>
2024-02-10 18:15:02 +0000

    Add poppable shield

--- entities/health/shield.gd
+++ entities/health/shield.gd
@@ -6,8 +6,7 @@ class_name Shield
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$Health.connect("death", queue_free)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
