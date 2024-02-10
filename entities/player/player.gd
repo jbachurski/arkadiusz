d6095b9045544f33c3636283c74a6171a6f48993
jbachurski <kbachurski@gmail.com>
2024-02-10 12:43:22 +0000

    Clip player position

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -5,3 +5,6 @@ const SPEED = 500.0
 func _process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	position = position + delta * direction * SPEED
+	var size = get_viewport_rect().size
+	position.x = clamp(position.x, 0, size.x)
+	position.y = clamp(position.y, 0, size.y)
