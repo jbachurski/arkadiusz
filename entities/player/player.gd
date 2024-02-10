01d2f06fd4601b2f7ed93a059ca8717516c57105
jbachurski <kbachurski@gmail.com>
2024-02-10 14:07:32 +0000

    Lower player speed

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,10 +1,11 @@
 extends PlayerBase
 
-const SPEED = 500.0
+var speed = 300.0
+var speed_level = 0
 
 func _process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position = position + delta * direction * SPEED
+	position = position + delta * direction * speed * pow(1.25, speed_level)
 	var size = get_viewport_rect().size
 	position.x = clamp(position.x, 0, size.x)
 	position.y = clamp(position.y, 0, size.y)
