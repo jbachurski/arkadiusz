9acd075df87901a76c96527f96b1223f263e1094
jbachurski <kbachurski@gmail.com>
2024-02-10 14:07:28 +0000

    Reflected pest waves

--- entities/enemies/enemies.gd
+++ entities/enemies/enemies.gd
@@ -2,6 +2,7 @@ extends CanvasGroup
 
 const PEST = preload("res://entities/enemies/pest.tscn")
 const ENEMY = preload("res://entities/enemies/enemy.tscn")
+@onready var size = get_viewport_rect().size
 
 class Wave:
 	var delay
@@ -15,21 +16,37 @@ class Wave:
 		await fun.call()
 
 var WAVES = [
-	Wave.new(2.5, wave1),
+	Wave.new(0.5, wave1),
 	Wave.new(7.5, wave2)
 ]
 
 func sleep(seconds: float) -> void:
 	await get_tree().create_timer(seconds).timeout
 
+
+func next_pest(pos):
+	var e = PEST.instantiate()
+	e.position = pos
+	return e
+
+func next_pest_refl(pos):
+	var e = next_pest(pos)
+	e.curve = e.curve.duplicate()
+	for p in range(e.curve.get_point_count()):
+		e.curve.set_point_position(p, Vector2(-1, 1) * e.curve.get_point_position(p))
+	return e
+
 func wave1():
+	add_child(next_pest_refl(Vector2(0.5, 0) * size))
 	for i in range(5):
-		var e = PEST.instantiate()
-		var size = get_viewport_rect().size
-		var pos = Vector2(0.2, -0.1) * size
-		e.position = pos
-		add_child(e)
-		await sleep(0.4)
+		var pos = Vector2(0.25, -0.1) * size
+		add_child(next_pest(pos))
+		await sleep(0.5)
+	await sleep(2.5)
+	for i in range(5):
+		var pos = Vector2(0.75, -0.1) * size
+		add_child(next_pest_refl(pos))
+		await sleep(0.5)
 
 func wave2():
 	var e = ENEMY.instantiate()
