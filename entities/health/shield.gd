ebcd9db65e4ebdbaf057363f828d595fa4ad064d
spdskatr <25078279+spdskatr@users.noreply.github.com>
2024-02-10 20:33:50 +0000

    Shields are now poppable

--- entities/health/shield.gd
+++ entities/health/shield.gd
@@ -4,8 +4,12 @@ class_name Shield
 @export var health: Health
 @export var area: CollisionShape2D
 
+func _on_death():
+	self.queue_free()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Health.connect("death", _on_death)
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
