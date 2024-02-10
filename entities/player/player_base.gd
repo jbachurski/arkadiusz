3893ea4cf4842861968f04a6e09b366374b2340c
spdskatr <25078279+spdskatr@users.noreply.github.com>
2024-02-10 16:15:06 +0000

    Increase firepower

--- entities/player/player_base.gd
+++ entities/player/player_base.gd
@@ -6,4 +6,4 @@ const MISSILE = preload("res://entities/projectiles/missile.tscn")
 func shoot():
 	var b = MISSILE.instantiate()
 	get_parent().get_parent().add_child(b)
-	b.start(position, Vector2(0, -1), 500, 1, ProjectileBase.TEAM.PLAYER)
+	b.start(position, Vector2(0, -1), 1000, 1, ProjectileBase.TEAM.PLAYER)
