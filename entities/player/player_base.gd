934851e4fc65e61c8d2d63a2fc807b4dbf5d30de
Rokcas <rokas0090@gmail.com>
2024-02-10 15:59:20 +0000

    Make shooting time shared between players

--- entities/player/player_base.gd
+++ entities/player/player_base.gd
@@ -3,11 +3,7 @@ class_name PlayerBase
 
 const MISSILE = preload("res://entities/projectiles/missile.tscn")
 
-func _on_shoot_timer_timeout():
+func shoot():
 	var b = MISSILE.instantiate()
 	get_parent().get_parent().add_child(b)
 	b.start(position, Vector2(0, -1), 500, 1, ProjectileBase.TEAM.PLAYER)
-
-func _ready():
-	$ShootTimer.connect("timeout", _on_shoot_timer_timeout)
-
