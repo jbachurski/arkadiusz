c8828eb1cdc3f7a10b37d05f0d02da105f3b186f
spdskatr <25078279+spdskatr@users.noreply.github.com>
2024-02-10 19:59:46 +0000

    Remove debug prints

--- entities/health/health.gd
+++ entities/health/health.gd
@@ -10,7 +10,6 @@ var health: int = 0
 
 func _ready():
 	health = max_health
-	print(get_parent(), " my team is ", team)
 
 func is_immune(proj_team: ProjectileBase.Team):
 	return team == proj_team
@@ -18,7 +17,6 @@ func is_immune(proj_team: ProjectileBase.Team):
 func deal_damage(val: int, proj_team: ProjectileBase.Team) -> bool:
 	if is_immune(proj_team):
 		return false
-	print(team, " took damage from ", proj_team)
 	health -= val
 	if health <= 0:
 		emit_signal("death")
