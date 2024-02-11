25dc915f276c1b2408d0a20e721a29a7a662e9c7
Rokcas <rokas0090@gmail.com>
2024-02-11 14:03:04 +0000

    Add health regeneration

--- entities/health/health.gd
+++ entities/health/health.gd
@@ -5,8 +5,8 @@ signal death
 signal damage
 
 @export var team: ProjectileBase.Team
-@export var max_health: int = 1
-var health: int = 0
+@export var max_health: float = 1
+var health: float = 0
 var stop_its_already_dead: bool = false
 
 func _ready():
@@ -19,7 +19,7 @@ func is_immune(proj_team: ProjectileBase.Team):
 			return true
 	return team == proj_team
 
-func deal_damage(val: int, proj_team: ProjectileBase.Team) -> bool:
+func deal_damage(val: float, proj_team: ProjectileBase.Team) -> bool:
 	if is_immune(proj_team) or stop_its_already_dead:
 		return false
 	if has_node("Invframe"):
--- entities/player/player_health.gd
+++ entities/player/player_health.gd
@@ -1,5 +1,5 @@
 extends Health
 class_name PlayerHealth
 
-func deal_damage(val: int, proj_team: ProjectileBase.Team):
+func deal_damage(val: float, proj_team: ProjectileBase.Team):
 	return self.get_parent().get_parent().find_child("Health", false).deal_damage(val, proj_team)
